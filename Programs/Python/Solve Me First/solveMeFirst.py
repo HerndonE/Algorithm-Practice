ffcf0a9d75caf35e48cb4aec44b6201e89da2bc3
HerndonE <EHerndon17@gmail.com>
2021-11-25 15:17:56 -0300

    [add] solve me first - python

---
+++ Programs/Python/Solve Me First/solveMeFirst.py
@@ -0,0 +1,15 @@
+"""
+name: Ethan Herndon
+date: 11/25/2021
+https://www.hackerrank.com/challenges/solve-me-first/problem
+"""
+
+def solveMeFirst(a,b):
+	answer = a + b
+	return(answer)
+
+
+num1 = int(input())
+num2 = int(input())
+res = solveMeFirst(num1,num2)
+print(res)
