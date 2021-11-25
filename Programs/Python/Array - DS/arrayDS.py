40dd9cf5714dd6eb4782157351a758ebe9c1a8f3
HerndonE <EHerndon17@gmail.com>
2021-11-25 17:48:11 -0300

    [add] Array - DS python

---
+++ Programs/Python/Array - DS/arrayDS.py
@@ -0,0 +1,39 @@
+"""
+name:Ethan Herndon
+date: 11/25/2021
+https://www.hackerrank.com/challenges/arrays-ds/problem 
+"""
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'reverseArray' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY a as parameter.
+#
+
+def reverseArray(a):
+    reverseNums = a
+    reverseNums.reverse()
+    return(reverseNums)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    arr_count = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    res = reverseArray(arr)
+
+    fptr.write(' '.join(map(str, res)))
+    fptr.write('\n')
+
+    fptr.close()
+
